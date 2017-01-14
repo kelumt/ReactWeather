e087a8c90d521ed8eedc230a5a10fe93af1d9297
Kelum Thenuwara <kelum.thenuwara@gmail.com>
2017-01-14 11:20:55 +0530

    Add Grid system and style Examples page

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,5 +1,6 @@
 var React = require('react');
-
+var {Link} = require('react-router');
+/*
 var Examples = React.createClass({
 
   render: function(){
@@ -9,10 +10,22 @@ var Examples = React.createClass({
   }
 
 });
-
-var Example = (props) => {
+*/
+var Examples = (props) => {
   return (
-    <h2>Examples Component</h2>
+    <div>
+      <h1 className="text-center">Examples</h1>
+      <p>Here are a few example locations to try out: </p>
+      <ol>
+        <li>
+          <Link to="/?location=colombo">Colombo, SL</Link>
+        </li>
+        <li>
+          <Link to="/?location=Toronto">Toronto, CA</Link>
+        </li>
+      </ol>
+    </div>
+
   );
 };
 
--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -16,12 +16,27 @@ var Main = React.createClass({
 
 });
 */
+/*
 var Main = (props) => {
   return(
     <div>
       <Nav/>
       <h2>Hello Main Component</h2>
       {props.children}
+    </div>
+  );
+};
+*/
+
+var Main = (props) => {
+  return(
+    <div>
+      <Nav/>
+      <div className="row">
+        <div className="column medium-6 large-4 small-centered">
+          {props.children}
+        </div>
+      </div>
     </div>
   );
 };
